4b6645ce98823ec74fd68516d8c97d975c4ab00b
sogabe <shucengwobu65@gmail.com>
2022-09-18 03:40:27 +0900

    add
    Google Mapの実装

--- lib/city.dart
+++ lib/city.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'google_map_api.dart';
 
 class City extends StatelessWidget {
   const City({super.key});
@@ -10,11 +11,40 @@ class City extends StatelessWidget {
         backgroundColor: const Color(0xff66B6C0),
         elevation: 0,
       ),
-      body: Container(
-        color: const Color(0xff66B6C0),
-        child: Column(),
+      body: Column(
+        children: [
+          StaticMapView(),
+        ],
       ),
       backgroundColor: const Color(0xff66B6C0),
+    );
+  }
+}
+
+class StaticMapView extends StatelessWidget {
+  static const _baseMapURL = 'https://maps.googleapis.com/maps/api/staticmap';
+  static const _latitude = 35.667097;
+  static const _longitude = 139.740178;
+  static const _mapZoom = 'zoom=18';
+  static const _mapSize = 'size=720x640';
+  static const _scale = 'scale=2';
+  static const _language = 'language=ja';
+  static const _mapCenter = 'center=$_latitude,$_longitude';
+  static const _mapMarkers = 'markers=$_latitude,$_longitude';
+  static const _apiKey = 'key=${ApiKey.googleApi}';
+  static const _imageUrl =
+      '$_baseMapURL?$_mapCenter&$_mapZoom&$_mapMarkers&$_mapSize&$_scale&$_language&$_apiKey';
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      child: Center(
+        child: Image.network(
+          _imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
